7fd4f629be0d029b4bf91d09a0e32695540655fa
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-01 18:42:11 -0700

    Add API endpoint and refactor server.js

--- Exmaples/React-Node/backend/server.js
+++ Exmaples/React-Node/backend/server.js
@@ -1,25 +1,4 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('dist'));
-
-app.get('/', (_req, res) => {
-    res.send('Hello World');
-});
-
-app.get('/api/users', (_req, res) => {    
-    res.send([{
-        id: 1,
-        name: 'John',
-        age: 22
-    },
-    {
-        id: 2,
-        name: 'Jane',
-        age: 23
-    }
-    ]);
-});
+const app = require('./index');
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
