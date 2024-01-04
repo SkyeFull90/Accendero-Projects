48a8c10f328fc084340a9e706585c8f045df30b2
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-04 11:02:36 -0700

    Add cors dependency and configure CORS settings in server.js

--- Exmaples/React-Node/backend/server.js
+++ Exmaples/React-Node/backend/server.js
@@ -1,4 +1,18 @@
-const app = require('./index');
+const express = require('express');
+const userRoutes = require('./routes/userRoutes')
+const app = express();
+const cors = require('cors');
+//app.use(express.static('dist'));
+app.use('/api/users', userRoutes);
+
+const corsOptions = (
+    process.env.NODE_ENV === 'development' ?
+        { origin: 'http://localhost:3000' } :
+        { origin: 'https://my-app.com' }
+);  
+app.use(cors(corsOptions));
+
+app.use(cors());
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
