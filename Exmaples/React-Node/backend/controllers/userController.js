51931e30b03af30739588aba56a6fc4b2d294948
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-04 08:23:04 -0700

    Add userController and userRoutes files

---
+++ Exmaples/React-Node/backend/controllers/userController.js
@@ -0,0 +1,12 @@
+exports.getUsers = (req, res) => {
+    res.send([{
+        id: 1,
+        name: 'John',
+        age: 22
+    },
+    {
+        id: 2,
+        name: 'Jane',
+        age: 23
+    }]);
+};
