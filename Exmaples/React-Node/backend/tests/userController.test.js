66ce24879db826c4e82a6de3eb00b4339c1e41db
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-04 10:00:30 -0700

    Update backend dependencies and add userController test

--- Exmaples/React-Node/backend/tests/userController.test.js
+++ Exmaples/React-Node/backend/tests/userController.test.js
@@ -1,9 +1,17 @@
 const userController = require('../controllers/userController');
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
 describe('UserController', () => {
     it('should return all users', () => {
-        const users = userController.getUsers();
+        const req = {};
+        const res = {
+            send: sinon.spy()
+        };
+
+        userController.getUsers(req, res);
+        expect(res.send.calledOnce).to.be.true;
+        const users = res.send.firstCall.args[0];
         expect(users).to.be.a('array');
         expect(users.length).to.be.eql(2);
         expect(users[0]).to.have.property('name').eql('John');
