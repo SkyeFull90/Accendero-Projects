ca54bff88c608f5a08d1e7ef5344167961370006
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2024-01-04 12:59:11 -0700

    Add new files and update configurations

--- Exmaples/React-Node/backend/server.js
+++ Exmaples/React-Node/backend/server.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes')
 const app = express();
-const cors = require('cors');
-//app.use(express.static('dist'));
-app.use('/api/users', userRoutes);
+app.use(express.static('dist'));
+app.use('/', userRoutes);
 
-const corsOptions = (
-    process.env.NODE_ENV === 'development' ?
-        { origin: 'http://localhost:5173' } :
-        { origin: 'https://my-app.com' }
-);  
-app.use(cors(corsOptions));
-
-app.use(cors());
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
--- Exmaples/React-Node/frontend/vite.config.ts
+++ Exmaples/React-Node/frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      '/api/users': {
         target: 'http://localhost:8080',
       },
     },
